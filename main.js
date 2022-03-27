be3503d64414bff6f3eb9ea0d387df1069ca2b0e
Caio de Alcantara Santos <caioalcantarasantos3@gmail.com>
2022-03-27 20:53:33 -0300

    Adds JS, form validation and final details

---
+++ main.js
@@ -0,0 +1,34 @@
+const button = document.querySelector(".button");
+
+
+function validateEmail() {
+  let regex = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  let email = document.querySelector("#email").value;
+  if (email.match(regex)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  
+  if (validateEmail() == false) {
+    let email = document.querySelector("#email").style = 'border: 1px solid red;'
+    
+    let errorIcon = document.querySelector('.error').style = 'display: block;'
+
+    let errorMessage = document.querySelector('.error-message').style = 'display: block;'
+    return
+  }
+
+  if (validateEmail() == true) {
+    let email = document.querySelector("#email").style = 'border: 1px solid green;'
+
+    let errorMessage = document.querySelector('.error-message')
+    errorMessage.style = 'display: block; color: green; padding-left: -20px;'
+    errorMessage.textContent = 'Email sent sucessfuly!'
+  }
+  
+});
