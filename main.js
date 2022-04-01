00a6430367e2940261a16b0e6e24e4db17c46a08
Caio de Alcantara Santos <caioalcantarasantos3@gmail.com>
2022-03-31 22:58:21 -0300

    Fix image's position

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const button = document.querySelector(".button");
 
-
 function validateEmail() {
   let regex = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
   let email = document.querySelector("#email").value;
@@ -11,24 +10,31 @@ function validateEmail() {
   }
 }
 
+
+
 button.addEventListener("click", function (event) {
   event.preventDefault();
-  
-  if (validateEmail() == false) {
-    let email = document.querySelector("#email").style = 'border: 1px solid red;'
-    
-    let errorIcon = document.querySelector('.error').style = 'display: block;'
 
-    let errorMessage = document.querySelector('.error-message').style = 'display: block;'
-    return
+  if (validateEmail() == false) {
+    let email = (document.querySelector("#email").style =
+      "border: 1px solid red;");
+
+    let errorIcon = (document.querySelector(".error").style =
+      "display: block;");
+
+    let errorMessage = document.querySelector(".error-message");
+    errorMessage.style = "display: block; color: red;";
+    errorMessage.innerText = "Please provide a valid email!";
+
+    return;
   }
 
   if (validateEmail() == true) {
-    let email = document.querySelector("#email").style = 'border: 1px solid green;'
-    let errorIcon = document.querySelector('.error').style = 'display: none;'
-    let errorMessage = document.querySelector('.error-message')
-    errorMessage.style = 'display: block; color: green; padding-left: -20px;'
-    errorMessage.textContent = 'Email sent sucessfuly!'
+    let email = (document.querySelector("#email").style =
+      "border: 1px solid green;");
+    let errorIcon = (document.querySelector(".error").style = "display: none;");
+    let errorMessage = document.querySelector(".error-message");
+    errorMessage.style = "display: block; color: green; padding-left: -20px;";
+    errorMessage.textContent = "Email sent sucessfuly!";
   }
-  
 });
